97e581855540fe13373e0a72b62c90438f4859fc
Andi Mititelu <andi.mititelu123@gmail.com>
2023-11-11 11:45:31 +0200

    Fixed project

--- PropertyListing/Server/Domain/Interfaces/IUserRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿using PropertyListing.Server.Domain.Entities;
-
-namespace PropertyListing.Server.Domain.Interfaces
-{
-    public interface IUserRepository : IGenericRepository<User>
-    {
-
-    }
-}
